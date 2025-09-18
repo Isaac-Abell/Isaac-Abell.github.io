adfe3fbb868454d317ccc185902909b88c905e50
Isaac-A-UofT <isaac.abell@mail.utoronto.ca>
2025-09-17 23:44:55 -0400

    make better

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,18 @@ import { Projects } from './components/projects'
 import { Experience } from './components/experience'
 import { Bio } from './components/bio'
 import { socials, projects, experiences, bioData } from './data'
+import headshot from './assets/headshot.png';
+
 import './App.css'
 
 function App() {
-
   return (
     <>
-      <TitleBanner socials={socials}/>
+      <div className="header-container">
+        <img src={headshot} alt="Headshot" className="bio-headshot" />
+        <TitleBanner socials={socials}/>
+      </div>
+
       <Bio bioData={bioData}/>
       <div className='items'>
         <Experience experiences={experiences}/>
